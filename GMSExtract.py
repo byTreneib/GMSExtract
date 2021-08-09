3fdbf4051c7e7dc4f52a65a05d666c4f8fe1706e
byTreneib <51266397+byTreneib@users.noreply.github.com>
2021-08-09 15:36:01 +0200

    Added option to read multiple input files using '*' in filename. Inserted loop to add option to read another input after previous is finished. Program can be terminated with 'quit' keyword. Changes commented and basic testing performed.

--- GMSExtract.py
+++ GMSExtract.py
@@ -39,8 +39,9 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.layout import LAParams
-from typing import List
+from typing import List, Tuple
 from io import StringIO
+from glob import glob
 import re
 
 
@@ -78,13 +79,28 @@ class GMSExtract:
                                       check_extractable=True):
             interpreter.process_page(page)
 
-        text = output_string.getvalue()
+        content_text = output_string.getvalue()
 
         fp.close()
         device.close()
         output_string.close()
 
-        return text
+        return content_text
+
+    @staticmethod
+    def read_pdf_multiple(filenames: List[str]) -> List[str]:
+        """
+        Read contents of all files in filenames
+        :param filenames: List containing filenames and paths to pdf files
+        :return: List containing the contents of each of the passed pdf files as strings
+        """
+        file_contents: List[str] = []
+
+        for filename in filenames:
+            file_contents.append(GMSExtract.read_pdf(filename))
+
+        return file_contents
+
 
     @staticmethod
     def normalize_string(string: str) -> str:
@@ -117,55 +133,89 @@ class GMSExtract:
     def match_euh(string: str) -> List[str]:
         """
         Find all matches for EUH-Statements in the given input string using regular expressions
-        :param string: input string containing H-Statements
+        :param string: input string containing EUH-Statements
         :return: list of unique EUH-Statements as strings
         """
         return list(set(re.findall(GMSExtract.euh_pattern, string)))
 
+    @staticmethod
+    def process(string: str) -> Tuple[List[str], List[str], List[str]]:
+        """
+        Find all matches for H-/P-/EUH-Statements in the given input string.
+        :param string: input string containing H-/P-/EUH-Statements
+        :return: tuple of three lists containing the H-/P-/EUH-Statements as described in the designated methods
+        """
+        normalized_string: str = GMSExtract.normalize_string(string)
 
-def get_input() -> str:
+        h_match = GMSExtract.match_h(normalized_string)
+        p_match = GMSExtract.match_p(normalized_string)
+        euh_match = GMSExtract.match_euh(normalized_string)
+
+        return h_match, p_match, euh_match
+
+    @staticmethod
+    def print_excel(h_match: List[str], p_match: List[str], euh_match: List[str], filename: str) -> None:
+        """
+        Print the H-/P-/EUH-Statements in a manner that allow the output string to be copy + pasted into excel
+        :param h_match: List containing all matches for H-Statements as described in match_h
+        :param p_match: List containing all matches for P-Statements as described in match_p
+        :param euh_match: List containing all matches for EUH-Statements as described in match_euh
+        :param filename: Name of the file the H-/P-/EUH-Statements were taken from. Empty if not from file
+        """
+        if len(h_match) + len(p_match) + len(euh_match) == 0:
+            print(f"\nFinished extracting. No Statements were found{(' in ' + filename) if filename != '' else ''}.")
+            return
+
+        prefix = filename + ": \t" if filename != "" else ""
+        print("\nFinished extracting. The following line can be copy + pasted into excel.")
+        print(prefix + "\t".join([", ".join(h_match), ", ".join(p_match), ", ".join(euh_match)]))
+
+
+def get_input() -> Tuple[List[str], List[str]]:
     """
     Read text from input prompt until an empty line is sent and the input up to this point is non-empty.
     If the text input is recognized to be a path to a pdf file, there will be an attempt to open the file
     and return its contents as text. If the attempt fails or no path was recognized, the input text will
     be returned.
-    :return: Plain input text or contents of passed pdf file as text
+    :return: Tuple of a list of the plain input texts or contents of passed pdf file(s) as text and the filenames
     """
     input_buffer = ""
 
-    print("Paste text or insert path to pdf. Finish input with empty line.")
+    print("Paste text or insert path to pdf ('*' may be used in filename). Finish input with empty line.")
     input_read: str = input("> ")
     input_buffer += input_read
 
     while input_read != "" or input_buffer.strip() == "":
+        if input_read == "quit":
+            print("\nExit keyword detected. Terminating.")
+            quit()
+
         input_read = input("> ")
         input_buffer += input_read + " "
 
     input_buffer = input_buffer.strip()
 
     if input_buffer.split(".")[-1] == "pdf":
-        print("\nInterpreting input as path to pdf file.")
+        print("\nInterpreting input as path to pdf file(s).")
         try:
-            return GMSExtract.read_pdf(input_buffer)
+            file_list = glob(input_buffer)
+
+            if len(file_list) == 0:
+                raise FileNotFoundError("No files found.")
+
+            return GMSExtract.read_pdf_multiple(file_list), file_list
         except FileNotFoundError:
             print("File could not be found. Interpreting input as plain text.")
-    return input_buffer
+    return [input_buffer], [""]
 
 
 if __name__ == '__main__':
-    text = get_input()
-    normalized_text = GMSExtract.normalize_string(text)
+    while True:
+        text_inputs, input_files = get_input()
 
-    h_match = GMSExtract.match_h(normalized_text)
-    p_match = GMSExtract.match_p(normalized_text)
-    euh_match = GMSExtract.match_euh(normalized_text)
+        print("\n" + "#" * 150)
 
-    if len(h_match) + len(p_match) + len(euh_match) == 0:
-        print("\nFinished extracting. No Statements were found.")
-        input("\nPress ENTER to terminate.")
-        quit()
+        for name, text in zip(input_files, text_inputs):
+            GMSExtract.print_excel(*GMSExtract.process(text), name)
 
-    print("\nFinished extracting. The following line can be copy + pasted into excel.")
-    print("\t".join([", ".join(h_match), ", ".join(p_match), ", ".join(euh_match)]))
-
-    input("\nPress ENTER to terminate.")
+        print("\n" + "#" * 150 + "\n")
